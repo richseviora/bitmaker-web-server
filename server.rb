1d031a5fce9615ad8f150e984b50475f84837d94
Rich Seviora <richard.seviora@gmail.com>
2014-10-27 15:26:59 -0400

    Updates
    Refactoring fix and comments.

--- server.rb
+++ server.rb
@@ -1,13 +1,18 @@
-require 'socket'                                    # Require socket from Ruby Standard Library (stdlib)
+require 'socket'
 
+# Response class is responsible for creating the response string, given a response type (required) and body (optional).
 class Response
   def initialize(response_type, response_body)
+    # Fail if I do not have a valid response type.
     raise 'Invalid Response Type' if (response_type != :success && response_type != :fail)
     @response_type = response_type
     @response_body = response_body
   end
 
+  #Overriding to_s method so it can take advantage of the default behaviour.
   def to_s
+    #Applying to_s to the response body as it can be a nil object.
+    #String + nil will fail. String + nil.to_s == String.
     header + @response_body.to_s
   end
 
@@ -32,17 +37,16 @@ class Response
 end
 
 class Server
-
+  # Initializes and starts the server.
   def initialize
     host = 'localhost'
     port = 2000
     server = TCPServer.open(host, port)
     puts "Server started on #{host}:#{port} ..."
-    response_handler(server)
+    execution_loop(server)
   end
 
-
-
+  # Main Server Exection Loop
   def execution_loop(server)
     loop do
       # Start Listening
@@ -73,18 +77,23 @@ class Server
     end
   end
 
-  def get_filename(lines)
-    lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')
+  # Method Parses the File Name given an array of response lines.
+  def get_filename(response_lines)
+    response_lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')
   end
 
+  # Method parses the request and returns an array of strings (one string per request line)
   def get_request(client)
     lines = []
+    #Checking EOF? as line = client.gets.chomp will fail when EOF.
+    #Keeping empty? check of line as it will loop until the session closes otherwise.
     while !client.eof? && (line = client.gets.chomp) && !line.empty?
       lines << line
     end
     lines
   end
 
+  # Reads and returns the file specified. Returns nil if filename does not match a file.
   def return_file(filename)
     if File.exists?(filename)
       File.read(filename)
